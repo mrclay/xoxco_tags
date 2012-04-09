b3a88388330e424606ab7d74fcee497e548c1680
Steve Clay <steve@mrclay.org>
2012-04-09 11:00:18 -0400

    handles pasting tag list containing comma(s)

--- views/default/xoxco_tags/js/elgg_after.php
+++ views/default/xoxco_tags/js/elgg_after.php
@@ -6,7 +6,19 @@ elgg.provide('elgg.tags');
 
 elgg.tags.init = function() {
     var time = (new Date).getTime(),
-        i = 0;
+        i = 0,
+        tidyTags = function(e){
+            var tags = ($(e.target).val() + ',' + e.tags).split(',');
+            var target = $(e.target);
+            target.importTags('');
+            for(var i = 0, z = tags.length; i<z; i++){
+                var tag = $.trim(tags[i]);
+                if(!target.tagExist(tag)){
+                    target.addTag(tag);
+                }
+            }
+            $('#' + target[0].id + '_tag').trigger('focus');
+        };
     $('.elgg-input-tags').each(function () {
         if (! this.id) {
             this.id = 't' + time + '_' + i++;
@@ -15,7 +27,12 @@ elgg.tags.init = function() {
         width: '98%',
         height: 'auto',
         placeholderColor:'#999999',
-        defaultText:'add a tag (separate with commas)'
+        defaultText:'add a tag (separate with commas)',
+        onAddTag: function (tag) {
+            if (tag.indexOf(',') > 0) {
+                tidyTags({target: this, tags: tag});
+            }
+        }
     });
 };
 
